f7fd7901f69580be9af81671d605837b230991a2
Aleksei <toporov.aleksei@gmail.com>
2022-12-23 01:51:56 +0300

    fix: minicssplugin error

--- src/components/templates/page.ts
+++ src/components/templates/page.ts
@@ -1,30 +1,30 @@
-//import './page.sass'
+import "./page.scss";
 
 abstract class Page {
   protected container: HTMLElement;
   static TextObject = {};
 
   constructor(id: string) {
-    this.container = document.createElement('div');
+    this.container = document.createElement("div");
     this.container.id = id;
   }
 
   protected createHeader() {
-    const header = document.createElement('header');
-    header.classList.add('header');
+    const header = document.createElement("header");
+    header.classList.add("header");
 
-    const headerTitle = document.createElement('h1');
-    headerTitle.innerText = 'ONLINE-Store';
-    headerTitle.classList.add('header-title');
+    const headerTitle = document.createElement("h1");
+    headerTitle.innerText = "ONLINE-Store";
+    headerTitle.classList.add("header-title");
     header.append(headerTitle);
 
-    const totalPrice = document.createElement('div');
-    totalPrice.classList.add('total-price');
-    totalPrice.innerText = '$5.2'
+    const totalPrice = document.createElement("div");
+    totalPrice.classList.add("total-price");
+    totalPrice.innerText = "$5.2";
     header.append(totalPrice);
 
-    const cart = document.createElement('div');
-    cart.classList.add('cart');
+    const cart = document.createElement("div");
+    cart.classList.add("cart");
     cart.innerText = "Cart";
     header.append(cart);
 
@@ -33,7 +33,7 @@ abstract class Page {
   }
 
   protected createHeaderTitle(text: string) {
-    const pageTitle = document.createElement('p');
+    const pageTitle = document.createElement("p");
     pageTitle.innerText = text;
     return pageTitle;
   }
