275a5b8b240ccafc08cb5f502ab16d67f28fd8a8
Nikita Ilyanov <nikita.glk@mail.ru>
2022-12-25 23:52:41 +0300

    feat: add links to footer

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,17 +1,58 @@
 import Component from "../../components/templates/components";
 
 class Footer extends Component {
-  static TextObject = {
-    MainTitle: "Footer",
-  };
 
   constructor() {
     super("div", "footer__wrapper", "footer");
   }
 
+  gitHubLinks() {
+    const gitLinksContainer = document.createElement('div');
+    gitLinksContainer.classList.add('footer__git');
+
+    const gitItemOne = document.createElement('a')
+    gitItemOne.innerText = 'nike93';
+    gitItemOne.href = 'https://github.com/nike93';
+    gitLinksContainer.append(gitItemOne);
+
+    const gitImg = document.createElement('img');
+    gitImg.classList.add('git-logo');
+    //gitImg.src = '../../img/git-logo.png';
+    gitImg.src = 'https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo-768x432.png';        
+    gitLinksContainer.append(gitImg);
+
+    const gitItemTwo = document.createElement('a')
+    gitItemTwo.innerText = 'top-aleksei';
+    gitItemTwo.href = 'https://github.com/top-aleksei';
+    gitLinksContainer.append(gitItemTwo);
+
+    this.container.append(gitLinksContainer);
+  }
+
+  rsLinks() {
+    const rsLinksContainer = document.createElement('div');
+
+    const rsLink = document.createElement('a');
+    rsLink.classList.add('rs-link');
+    rsLink.href = 'https://rs.school/js/';
+
+    const rsImg = document.createElement('img');
+    rsImg.classList.add('rs-logo');
+    //gitImg.src = '../../img/git-logo.png';
+    rsImg.src = 'https://rollingscopes.com/images/logo_rs_text.svg';    
+    rsLink.append(rsImg);
+    rsLinksContainer.append(rsLink);
+
+    this.container.append(rsLinksContainer);
+
+  }
+
   render() {
-    const title = this.createTitle(Footer.TextObject.MainTitle);
-    this.container.append(title);
+    this.gitHubLinks();
+    const year = document.createElement('p');
+    year.innerText = '2022';
+    this.container.append(year);
+    this.rsLinks();
     return this.container;
   }
 }
