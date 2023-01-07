016b90de7d4ffcc2e7570cdb71fceecd3756a212
Aleksei <toporov.aleksei@gmail.com>
2023-01-07 04:14:22 +0300

    fix: remove unneceassry comments

--- src/components/main/filters/filtrationLogic.ts
+++ src/components/main/filters/filtrationLogic.ts
@@ -2,29 +2,14 @@ import { productItem } from './../../templates/types';
 import App from '../../../app/app';
 
 class FiltrationLogic {
-  // static filterDataCheckbox(data: productItem[]) {
-  //   const allBoxFilters = App.state.filters.checkboxes;
-  //   const result: productItem[] = [];
-  //   allBoxFilters.forEach((el) => {
-  //     const [key, value] = el.split('-');
-
-  //     data.forEach((item) => {
-  //       if (item[key as keyof productItem] == value) {
-  //         result.push(item);
-  //       }
-  //     });
-  //   });
-
-  //   return result.length > 0 ? result : data;
-  // }
   static filterDataCheckbox() {
     let filteredData = App.data.prod;
     const allBoxFilters = App.state.filters.checkboxes;
     for (const key of Object.keys(allBoxFilters) as Array<keyof productItem>) {
       console.log(allBoxFilters[key]);
-      filteredData = filteredData.filter((el) => 
-         allBoxFilters[key]?.includes(el[key] as keyof productItem)
-      );      
+      filteredData = filteredData.filter((el) =>
+        allBoxFilters[key]?.includes(el[key] as keyof productItem)
+      );
     }
     console.log(filteredData);
 
