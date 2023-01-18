f5a8ab305551bbe410b79a09f15946eac2e18fa6
Nikita Ilyanov <nikita.glk@mail.ru>
2023-01-19 00:18:09 +0300

    feat: init new tests

---
+++ src/tests/summary.test.ts
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+import {expect, test} from '@jest/globals';
+import Summary from '../app/pages/cart/summary';
+
+const testSummary = new Summary();
+
+test('test summary', () => {
+  expect(testSummary.renderTitle().textContent).toEqual('Summary');
+})
+
+test('test summary', () => {
+  expect(testSummary.renderTitle()).not.toBeNull();
+})
+
+test('test promo', () => {
+  expect(testSummary.renderPromoSuggestion('rs')).not.toBeNull();
+})
+
+test('Test renderNB', () => {
+  expect(testSummary.renderNB().textContent).not.toEqual('Promo');
+})
+
+test('Test renderNB', () => {
+  expect(testSummary.renderNB().textContent).toEqual('*Promo for test: "rs", "ny"');
+})
+
+test('Test renderBTN', () => {
+  expect(testSummary.renderBuyButton().classList.contains('button')).toEqual(true);
+})
